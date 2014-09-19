9b41dcc6b1cc0a77633fa494d133a95c7867c6fb
gvsunil <gvsunil@gmail.com>
2014-09-19 20:07:24 +0530

    Update packages for mdm.

---
+++ custom/pre_install_apt_packages.sh
@@ -0,0 +1,1 @@
+wget -q -O - "http://anonscm.debian.org/loggerhead/pkg-postgresql/postgresql-common/trunk/download/head:/apt.postgresql.org.s-20130224224205-px3qyst90b3xp8zj-1/apt.postgresql.org.sh" | bash
